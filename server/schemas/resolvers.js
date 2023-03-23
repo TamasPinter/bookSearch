8f906e7697e27782449a9ada69d82e7a9a7d6433
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-03-23 18:14:01 -0400

    resolvers finished

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,50 @@ const resolvers = {
         me: async (parent, { username }) => {
             return User.findOne({ username });
         },
-
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password } ) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect email!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Password!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            };
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        
+                
+            
         
     }
 }
